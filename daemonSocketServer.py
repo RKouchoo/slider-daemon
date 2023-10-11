db23f4788af18b6b677af7b283b5f28cf322c2f1
rkouchoo <r.kouchoo1@gmail.com>
2023-10-12 01:24:16 +1100

    threadded socket test

--- daemonSocketServer.py
+++ daemonSocketServer.py
@@ -7,7 +7,7 @@ addresses = []
 def startDaemonSocket(port, byteSize, maxConnections):
     # get the hostname
     host = socket.gethostname()
-    serverSocket = socket.socket()  # get instance
+    '''serverSocket = socket.socket()  # get instance
 
     serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serverSocket.bind((host, port))  # bind host address and port together
@@ -16,16 +16,24 @@ def startDaemonSocket(port, byteSize, maxConnections):
     conn, address = serverSocket.accept()  # accept new connection
 
     print("Connection from: " + str(address))
+'''
+    for c in range(maxConnections):
 
-    while True:
-        conn, address = serverSocket.accept()
-        conThread = threading.Thread(target=socketIngestData, args=(conn, address, byteSize))
+        localSocket = socket.socket()
+        localSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        localSocket.bind((host, port))  # bind host address and port together
+        localSocket.listen(15)
+        conn, target = localSocket.accept()
+
+        print(f"Connection from: {target}")
+
+        conThread = threading.Thread(target=socketIngestData, args=(conn, target, byteSize))
         conThread.start()
 
 
 
-def socketIngestData(con, host, bytes):
-    addresses.append(host)
+def socketIngestData(con, target, bytes):
+    addresses.append(target)
     while True:
         msg = con.recv(bytes).decode("utf-8")
         if not msg:
